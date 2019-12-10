8b9d42fe4691d6addc2ca7d786e23e08cb839d44
Tom Tobar <codetombomb@gmail.com>
2019-12-10 14:49:52 -0600

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name=(name_of_dog)
+    @name = name_of_dog
+  end
+  def name 
+    @name
+  end
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  def breed
+    @breed
+  end
+end
